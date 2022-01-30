ebae78f028915809fe7eaab8f12a0ee6b1832bbd
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-01-31 01:21:08 +0530

    updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cron = require('node-cron');
 require('dotenv').config();
 var Twit = require('twit');
+const axios = require('axios').default;
 
 const bodyParser = require('body-parser');
 
@@ -20,13 +21,43 @@ var T = new Twit({
 
 
 
-cron.schedule('*/10 * * * * *', function() {
-    T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-        console.log(data)
-      })
-    console.log('running a task every 1/2 minute');
-  });
+// cron.schedule('*/10 * * * * *', function() {
+//     T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//         console.log(data)
+//       })
+//     console.log('running a task every 1/2 minute');
+//   });
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+    axios.get('https://www.officeapi.dev/api/quotes/random')
+    .then(function (response) {
+        const data = response.data["data"];
+        const quote = data.content;
+        const author = data.character;
+        const text = `"${quote}" ~ ${author.firstname} ${author.lastname}\n\n(via-autotweet)`
+        T.post('statuses/update', { status: text }, function(err, data, response) {
+            console.log(data)
+          })
+      }).catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+ 
+  res.send("Hello!");
+    
+})
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running");
 })
+
+// .then(data => {
+//     const quote = data.quote;
+//   console.log(quote);
+//   const text = `"${quote}" ~ Kanye West\n\n(via-autotweet)`
+//   T.post('statuses/update', { status: text }, function(err, data, response) {
+//       console.log(data)
+//     })
+    
+// })
+// .catch(err => console.log(err))
